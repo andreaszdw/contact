7384ece916768260edce128db63faae564b52476
andreaszdw <andreaszdw@googlemail.com>
2017-06-26 10:32:35 +0200

    test

---
+++ src/de/keinpunkt/Contact.java
@@ -0,0 +1,29 @@
+package de.keinpunkt;
+
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class Contact extends JFrame {//inheriting JFrame
+
+    JFrame f;
+
+    // constructor
+    Contact(){
+
+        JButton b=new JButton("klick");//create button
+        b.setBounds(130,100,100, 40);
+
+        add(b);//adding button on frame
+        setSize(400,500);
+        setLayout(null);
+        setVisible(true);
+
+    }
+
+
+    public static void main(String[] args) {
+
+        new Contact();
+
+    }
+}
